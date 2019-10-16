d51cedb1d1df89db036661d33f959ea15d3c3d4b
Someone <metallolom86@gmail.com>
2019-10-16 19:45:14 +0300

    border imput

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,18 +1,33 @@
-import React from "react"
+import React from "react";
 import { Link } from "react-router-dom";
-
 
 function Card(props) {
   return (
     <div className="all-post-conteiner" key={props.id}>
-      {props.title ? <p><Link to={`/post/${props.id}`}> {props.title} </Link></p> : <p><Link to={`/post/${props.id}`}> Title </Link></p>}
+      {props.title ? (
+        <p>
+          <Link to={`/post/${props.id}`}> {props.title} </Link>
+        </p>
+      ) : (
+        <p>
+          <Link to={`/post/${props.id}`}> Title </Link>
+        </p>
+      )}
       {props.text ? <p>{props.text}</p> : null}
-      {props.images ? props.images.map(image=>{
-      return <img key={image._id} alt="img"src={`http://hipstagram.asmer.fs.a-level.com.ua/${image.url}`} className="all-post-img"></img>} 
-  ) : null}
+      {props.images
+        ? props.images.map(image => {
+            return (
+              <img
+                key={image._id}
+                alt="img"
+                src={`http://hipstagram.asmer.fs.a-level.com.ua/${image.url}`}
+                className="all-post-img"
+              ></img>
+            );
+          })
+        : null}
     </div>
-  )
+  );
 }
 
-
-export { Card }
+export { Card };
