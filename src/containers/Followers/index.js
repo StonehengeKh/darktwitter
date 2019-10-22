ce6c157dd28ead598f728abd141d9648b15c082e
StonehengeKh <alex.kharkov.ua@gmail.com>
2019-10-22 11:05:46 +0300

    fix head

--- src/containers/Followers/index.js
+++ src/containers/Followers/index.js
@@ -5,6 +5,8 @@ import { url, userUpsertFollowing } from "../../actions/user";
 import avatar from "../../assets/img/smile.jpg"
 import { Link } from "react-router-dom";
 
+import sample from './Up.mp4';
+
 
 class Followers extends Component {
 
@@ -25,27 +27,32 @@ class Followers extends Component {
   render() {
     const { followers, user} = this.props;
     return (
-      <div className="followers">
-        {followers && 
-          followers.map(userF => {
-            return <Link to={`users/${userF._id}`} key={userF._id}><div  className="user-wrap">
-              {userF.avatar ? (
-                <img src={url + userF.avatar.url} className="avatar-img" alt="avatar"/>
-              ) : (
-                <img
-                  src={avatar}
-                  className="avatar-img"
-                  alt="avatar"
-                />
-              )}
-              <p className="user-login">{userF.nick || userF.login}</p>
-             {user.following && user.following.some(user => user._id === userF._id) ?  null : <span className="followers-border icon-plus"
-              onClick={() => this.addFollowin(userF._id)} 
-             /> }
+        <div className="page-block">
+            <div className="followers">
+                {followers &&
+                  followers.map(userF => {
+                    return <Link to={`users/${userF._id}`} key={userF._id}><div  className="user-wrap">
+                      {userF.avatar ? (
+                        <img src={url + userF.avatar.url} className="avatar-img" alt="avatar"/>
+                      ) : (
+                        <img
+                          src={avatar}
+                          className="avatar-img"
+                          alt="avatar"
+                        />
+                      )}
+                      <p className="user-login">{userF.nick || userF.login}</p>
+                     {user.following && user.following.some(user => user._id === userF._id) ?  null : <span className="followers-border icon-plus"
+                      onClick={() => this.addFollowin(userF._id)}
+                     /> }
+                    </div>
+                    </Link>
+                })}
             </div>
-            </Link>
-        })}
-      </div>
+            <video className="videoTag" autoPlay loop muted>
+                <source src={sample} type='video/mp4' />
+            </video>
+        </div>
     );
   }
 }
