7c154afb39b4c9657b655665f121671ce0b5acfd
Someone <metallolom86@gmail.com>
2019-10-22 16:53:05 +0300

    add search length

--- src/containers/Search/index.js
+++ src/containers/Search/index.js
@@ -3,7 +3,7 @@ import { connect } from "react-redux";
 import { searchPosts, searchUser } from "../../actions/search";
 import Card from "../../components/Card";
 import { url, userUpsertFollowing } from "../../actions/user";
-import avatar from "../../assets/img/smile.jpg"
+import avatar from "../../assets/img/smile.jpg";
 import { Link } from "react-router-dom";
 
 import "./style.css";
@@ -17,17 +17,21 @@ const Search = ({
   userFail,
   userS,
   user,
-  userUpsertFollowing 
+  userUpsertFollowing
 }) => {
   const [searchPostValue, setSearchPostValue] = useState("");
   const [searchUserValue, setSearchUserValue] = useState("");
   const startSearchPost = () => {
-    searchPosts(searchPostValue);
-    setSearchPostValue("");
+    if (searchPostValue.length > 2) {
+      searchPosts(searchPostValue);
+      setSearchPostValue("");
+    }
   };
   const startSearchUser = () => {
-    searchUser(searchUserValue);
-    setSearchUserValue("");
+    if (searchUserValue.length > 2) {
+      searchUser(searchUserValue);
+      setSearchUserValue("");
+    }
   };
   const addFollowin = id => {
     let newFollowing = user.following
@@ -37,97 +41,98 @@ const Search = ({
           delete x.login;
           return x;
         })
-      : ( []);
+      : [];
     newFollowing.push({ _id: id });
     userUpsertFollowing(user.id, newFollowing);
   };
   return (
     <div className="search-block">
-        <div className="search-user-block">
-            {isFetching && <div>Loading...</div>}
-            <div className="block-search">
-                <h3>Search user</h3>
-                <input
-                    className="input"
-                    value={searchUserValue}
-                    onChange={event => setSearchUserValue(event.target.value)}
+         {isFetching && <div>Loading...</div>}
+      <div className="search-user-block">
+        <div className="block-search">
+          <h3>Search user</h3>
+          <input
+            className="input"
+            value={searchUserValue}
+            onChange={event => setSearchUserValue(event.target.value)}
+          />
+          <span className="button icon-search" onClick={startSearchUser} />
+          {userFail && <div>User not found</div>}
+        </div>
+        {userS && (
+          <Link to={`users/${userS._id}`}>
+            <div className="user-wrap">
+              {userS.avatar ? (
+                <img
+                  src={url + userS.avatar.url}
+                  className="avatar-img"
+                  alt="avatar"
                 />
-                <span className="button icon-search" onClick={startSearchUser} />
-                {userFail && <div>User not found</div>}
+              ) : (
+                <img src={avatar} className="avatar-img" alt="avatar" />
+              )}
+              <p className="user-login">{userS.nick || userS.login}</p>
+              {user.following &&
+              user.following.some(user => user._id === userS._id) ? null : (
+                <span
+                  className="followers-border icon-plus"
+                  onClick={() => addFollowin(userS._id)}
+                />
+              )}
             </div>
-            {userS && (
-                <Link to={`users/${userS._id}`}>
-                    <div className="user-wrap">
-                        {userS.avatar ? (
-                            <img
-                                src={url + userS.avatar.url}
-                                className="avatar-img"
-                                alt="avatar"
-                            />
-                        ) : (
-                            <img src={avatar} className="avatar-img" alt="avatar" />
-                        )}
-                        <p className="user-login">{userS.nick || userS.login}</p>
-                        {user.following &&
-                        user.following.some(user => user._id === userS._id) ? (
-                            null
-                        ) : (
-                            <span
-                                className="followers-border icon-plus"
-                                onClick={() => addFollowin(userS._id)}
-                            />
-                        )}
-                    </div>
-                </Link>
-            )}
+          </Link>
+        )}
+      </div>
+      <div className="search-page-block">
+        <div className="block-search">
+          <h3>Search post</h3>
+          <input
+            className="input"
+            value={searchPostValue}
+            onChange={event => setSearchPostValue(event.target.value)}
+          />
+          <span
+            className="button icon-search button-search"
+            onClick={startSearchPost}
+          />
+          {postsFail && <div>Post not found</div>}
         </div>
-        <div className="search-page-block">
-          <div className="block-search">
-            <h3>Search post</h3>
-            <input
-              className="input"
-              value={searchPostValue}
-              onChange={event => setSearchPostValue(event.target.value)}
-            />
-            <span className="button icon-search" onClick={startSearchPost} />
-            {postsFail && <div>Post not found</div>}
-          </div>
 
-      {posts &&
-        posts.map(post => {
-          const {
-            title,
-            text,
-            images,
-            owner,
-            createdAt,
-            comments,
-            likes,
-            _id
-          } = post;
-          return (
-            <Card
-              title={title}
-              key={_id}
-              id={_id}
-              images={images}
-              text={text}
-              avatar={owner.avatar}
-              nick={owner.nick}
-              login={owner.login}
-              createdAt={createdAt}
-              comments={comments}
-              likes={likes}
-              ownerId={owner._id}
-            />
-          );
-        })}
-        </div>
+        {posts &&
+          posts.map(post => {
+            const {
+              title,
+              text,
+              images,
+              owner,
+              createdAt,
+              comments,
+              likes,
+              _id
+            } = post;
+            return (
+              <Card
+                title={title}
+                key={_id}
+                id={_id}
+                images={images}
+                text={text}
+                avatar={owner.avatar}
+                nick={owner.nick}
+                login={owner.login}
+                createdAt={createdAt}
+                comments={comments}
+                likes={likes}
+                ownerId={owner._id}
+              />
+            );
+          })}
+      </div>
     </div>
   );
 };
 
-const mapStateToProps = ({ searchReducer , userReducer}) => {
+const mapStateToProps = ({ searchReducer, userReducer }) => {
   return {
     isFetching: searchReducer.isFetching,
     posts: searchReducer.posts,
@@ -140,5 +145,5 @@ const mapStateToProps = ({ searchReducer , userReducer}) => {
 
 export default connect(
   mapStateToProps,
-  { searchPosts, searchUser, userUpsertFollowing  }
+  { searchPosts, searchUser, userUpsertFollowing }
 )(Search);
